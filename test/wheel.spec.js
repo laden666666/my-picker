24c16f847e6647c383bf6e05672289933aea568a
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2018-03-18 01:20:09 +0800

    提交0.1.4。实现了对ie11，ie10的兼容

--- test/wheel.spec.js
+++ test/wheel.spec.js
@@ -3,7 +3,7 @@
  */
 var myPicker = require('../src/index.ts');
 
-describe('picker基本配置测试', function () {
+describe('非3D透视模式picker基本配置测试', function () {
 
     it('标准配置测试', function (done) {
         debugger
@@ -14,6 +14,7 @@ describe('picker基本配置测试', function () {
             }, {
                 options: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
             }],
+            isPerspective: false,
             onOkClick: function (values) {
                 assert.equal(values[0], '1月')
                 assert.equal(values[1], '星期一')
@@ -32,6 +33,7 @@ describe('picker基本配置测试', function () {
                 valueKey: 'name',
                 options: [{name: '张三'}, {name: '李四'}, {name: '王五'}],
             }],
+            isPerspective: false,
             onOkClick: function (values) {
                 assert.equal(values[0], '张三')
                 done()
@@ -49,6 +51,7 @@ describe('picker基本配置测试', function () {
             cols: {
                 options: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             },
+            isPerspective: false,
             onOkClick: function (values) {
                 assert.equal(values[0], '1月')
                 done()
@@ -64,6 +67,7 @@ describe('picker基本配置测试', function () {
         //单数数组测试
         window._picker = new myPicker({
             cols:  ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            isPerspective: false,
             onOkClick: function (values) {
                 assert.equal(values.length, 1)
                 assert.equal(values[0], '1月')
@@ -75,13 +79,14 @@ describe('picker基本配置测试', function () {
     });
 });
 
-describe('picker测试基本api', function () {
+describe('非3D透视模式picker测试基本api', function () {
     it('setValue测试', function (done) {
         //单数json测试
         window._picker = new myPicker({
             cols: {
                 options: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
             },
+            isPerspective: false,
             onOkClick: function (values) {
                 assert.equal(values[0], '2月')
                 done()
@@ -104,6 +109,7 @@ describe('picker测试基本api', function () {
             }, {
                 options: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
             }],
+            isPerspective: false,
             onOkClick: function (values) {
                 try {
                     assert.equal(values[0], '2月')
