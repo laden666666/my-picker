a5c917bcb630460071469aca8e08f9cf23321e90
laden666666 <njz198869@gmail.com>
2019-05-02 19:13:27 +0800

    fix #9

--- src/em.ts
+++ src/em.ts
@@ -3,11 +3,11 @@
  */
 
 export function em():number {
-    //根据屏幕的dpr判断em大小。如果dpr不是1，说明不是pc屏幕，此时选取window.innerWidth和window.innerHeight最小值的100分之一做1em。
-    //如果dpr是1，表示可能是pc屏幕，此时要求控件不能过大，所以去window.innerWidth、window.innerHeight、650当中的最小值的100分之一做1em
-    if(window.devicePixelRatio && window.devicePixelRatio > 1){
-        return Math.min(window.innerWidth, window.innerHeight) / 100;
+    // 如果height大于等于width，说明是pc或者手机的横屏模式，弹框显示在屏幕正中间
+    // 否则表示是手机竖屏，弹框显示在屏幕下面
+    if(window.innerWidth <= window.innerHeight){
+        return window.innerWidth / 100;
     } else {
-        return Math.min(window.innerWidth, window.innerHeight, 400) / 100;
+        return Math.min(window.innerHeight, 400) / 100;
     }
 }
